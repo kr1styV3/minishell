eaa6515d855a1fd80d256e7ab200a8d21c4b733a
kr1sty <ads.lombardi@gmail.com>
2025-04-11 18:43:55 +0200

    Revert "did somechange parsing perfect"
    This reverts commit 7cd5485e44140ef013db602db15b9d50b8965cf4.

--- src/executor.c
+++ src/executor.c
@@ -6,7 +6,11 @@
 /*   By: chrlomba <chrlomba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 16:04:12 by chrlomba          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2025/04/07 20:08:54 by chrlomba         ###   ########.fr       */
+=======
+/*   Updated: 2025/03/13 06:08:24 by coca             ###   ########.fr       */
+>>>>>>> parent of 7cd5485... did somechange parsing perfect
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -221,6 +225,187 @@ static t_token	*get_job_end(t_token *start)
 	return (job_end);
 }
 
+<<<<<<< HEAD
+=======
+/* Function to handle here_doc */
+int handle_here_doc(t_token *current)
+{
+    int pipe_fd[2];
+    char    *line;
+
+    line = NULL;
+    if (pipe(pipe_fd) == -1)
+    {
+        perror("pipe failed for here_doc");
+        should_exit = 1;
+    }
+    while (1)
+    {
+        line = readline(HERE_DOC_PROMT);
+        if (ft_strcmp(line, current->doc->eof) == 0)
+            break;
+        line = ft_strjoin(line, "\n");
+        if (write(pipe_fd[1], line, ft_strlen(line)) == -1)
+        {
+            perror("write failed for here_doc");
+            free(line);
+            should_exit = 1;
+        }
+        free(line);
+        line = NULL;
+    }
+    free(line);
+    close(pipe_fd[1]); // Close write end of the pipe
+    return pipe_fd[0]; // Return read end of the pipe
+}
+
+
+int execute_pipeline(t_token *job_start, t_token *job_end, char **env, bool background)
+{
+    int pipe_fd[2];
+    int prev_fd = -1;
+    pid_t pid;
+    pid_t child_pids[MAX_COMMANDS];
+    int child_count = 0;
+    int status = 0;
+    t_token *current = job_start;
+
+    while (current != job_end->next)
+    {
+        // Check if the current command is a builtin with output.
+        bool is_builtin_output = false;
+        if (current->arg && current->arg[0])
+        {
+            if (ft_strcmp(current->arg[0], "echo") == 0 ||
+                ft_strcmp(current->arg[0], "env") == 0 ||
+                ft_strcmp(current->arg[0], "export") == 0 ||
+                ft_strcmp(current->arg[0], "pwd") == 0)
+            {
+                is_builtin_output = true;
+            }
+        }
+
+        // Determine whether the current command is piped.
+        bool is_piped = (current != job_end && current->operator &&
+                         current->operator->operator == '|');
+
+        // If it's a builtin with output, print its output in the main process.
+        if (is_builtin_output)
+        {
+            int out_fd = STDOUT_FILENO;
+            // If the command is piped, create a pipe and write to its write end.
+            if (is_piped)
+            {
+                if (pipe(pipe_fd) == -1)
+                {
+                    perror("pipe failed");
+                    should_exit = 1;
+                }
+                out_fd = pipe_fd[1];
+            }
+            ft_putstr_fd(current->arg[1], out_fd);
+            // If we created a pipe, close its write end and pass along the read end.
+            if (is_piped)
+            {
+                close(pipe_fd[1]);
+                prev_fd = pipe_fd[0];
+            }
+            else if (prev_fd != -1)
+            {
+                close(prev_fd);
+                prev_fd = -1;
+            }
+            current = current->next;
+            continue;
+        }
+
+        // For non-builtins, set up piping if needed.
+        if (is_piped)
+        {
+            if (pipe(pipe_fd) == -1)
+            {
+                perror("pipe failed");
+                should_exit = 1;
+            }
+        }
+
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork failed");
+            should_exit = 1;
+        }
+        else if (pid == 0)
+        {
+            // If there is a previous pipe, redirect STDIN from it.
+            if (prev_fd != -1)
+            {
+                if (dup2(prev_fd, STDIN_FILENO) == -1)
+                {
+                    perror("dup2 prev_fd failed");
+                    should_exit = 1;
+                }
+                close(prev_fd);
+            }
+            // If the current command is piped, redirect STDOUT to the pipe.
+            if (is_piped)
+            {
+                close(pipe_fd[0]);
+                if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
+                {
+                    perror("dup2 pipe_fd[1] failed");
+                    should_exit = 1;
+                }
+                close(pipe_fd[1]);
+            }
+            // Handle here_doc if present.
+            if (current->doc->here_doc)
+            {
+                int here_doc_fd = handle_here_doc(current);
+                if (dup2(here_doc_fd, STDIN_FILENO) == -1)
+                {
+                    perror("dup2 here_doc_fd failed");
+                    should_exit = 1;
+                }
+                close(here_doc_fd);
+            }
+            setup_redirections(current);
+            execve(current->arg[0], current->arg, env);
+            perror("execve failed");
+            should_exit = 1;
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            child_pids[child_count++] = pid;
+            if (prev_fd != -1)
+                close(prev_fd);
+            if (is_piped)
+            {
+                close(pipe_fd[1]);
+                prev_fd = pipe_fd[0];
+            }
+            else
+            {
+                prev_fd = -1;
+            }
+        }
+        current = current->next;
+    }
+
+    // Wait for all children to finish if not running in background.
+    for (int i = 0; i < child_count; i++)
+    {
+        if (!background)
+            waitpid(child_pids[i], &status, 0);
+    }
+
+    return status;
+}
+
+
+
+>>>>>>> parent of 7cd5485... did somechange parsing perfect
 /* Main execution function */
 int	execute(t_token **token_list, char **env, t_env_list *env_list)
 {
--- src/main.c
+++ src/main.c
@@ -5,8 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: chrlomba <chrlomba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
+<<<<<<< HEAD
 /*   Created: 2025/04/06 20:34:25 by chrlomba          #+#    #+#             */
 /*   Updated: 2025/04/06 20:36:54 by chrlomba         ###   ########.fr       */
+=======
+/*   Created: Invalid date        by                   #+#    #+#             */
+/*   Updated: 2025/03/13 04:44:46 by coca             ###   ########.fr       */
+>>>>>>> parent of 7cd5485... did somechange parsing perfect
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +38,46 @@ int	main(int ac, char **av, char **envp)
 	head = (t_token **)malloc(sizeof(t_token *));
 	if (!head)
 		ft_error("Failed to allocate memory for token.");
+<<<<<<< HEAD
 	_env_ptr = env_copy(envp);
 	setup_signal_handling();
 	shell_loop(head, _env_ptr, envp, &status);
 	free_list_copy(_env_ptr);
+=======
+	// setup_signal_handling();
+	(void)ac;
+	(void)av;
+	while (!should_exit)
+	{
+		*head = init_token();
+		token = *head;
+		printf("last exitn status: %d\n", status);
+		token->last_exit_status = status;
+		read_line_from_user(&(*head), _env_ptr);
+		if (token->exec == true || should_exit)
+		{
+			while (token)
+			{
+				if (token->parsed->token)
+				{
+					if (checker(&token, _env_ptr) == 1 && !token->checker)
+					{
+						free_inside_token("minishell: command not found: ", token->parsed->token);
+						token->exec = false;
+					}
+					printf("%s\n", token->arg[1]);
+				}
+				token = token->next;
+			}
+			token = *head;
+			if (token->exec == true)
+				status = execute(head, envp, _env_ptr);
+		}
+		if (should_exit == 2)
+			should_exit = 0;
+		free_token(token);
+	}
+>>>>>>> parent of 7cd5485... did somechange parsing perfect
 	free(head);
 	rl_clear_history();
 	return (0);
